2d711128a99a6399aadc33e44f563429ca5c9ebf
David Burkett <davidburkett38@gmail.com>
2021-08-16 04:20:29 -0400

    Build fixes

--- src/undo.h
+++ src/undo.h
@@ -43,7 +43,7 @@ struct TxInUndoFormatter
             // Old versions stored the version number for the last spend of
             // a transaction's outputs. Non-final spends were indicated with
             // height = 0.
-            unsigned int nVersionDummy;
+            unsigned int nVersionDummy = 0;
             ::Unserialize(s, VARINT(nVersionDummy));
         }
         ::Unserialize(s, Using<TxOutCompression>(txout.out));
